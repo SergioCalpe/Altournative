760134e1c8c55511868202467bf63d88de529e60
Hector <sansanomiralles@Hectors-MacBook.local>
2016-03-10 12:49:50 +0100

    fafagag

---
+++ Frontend/web/home/home.js
@@ -0,0 +1,27 @@
+alert('Cargamos página de home');
+
+angular.module('altournative.home', [
+	'ui.router',
+	'angular-storage',
+	'angular-jwt'
+])
+.config(function($stateProvider) {
+	$stateProvider.state('home', {
+		url: '/',
+		controller: 'HomeCtrl',
+		templateUrl: 'home/home.html',
+		data: {
+			requiresLogin: false
+		}
+	});
+})
+.controller('HomeCtrl', function HomeController($scope, $http, store, jwtHelper, $location) {
+	$scope.signup = function() {
+		$location.path('/signup');
+	}
+
+	$scope.login = function() {
+		$location.path('/login');
+	}
+
+});
