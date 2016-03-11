e9c34f3afc191d8fb80503fb0ce24f9c5ad36e40
Fran <Fran>
2016-03-11 21:49:06 +0100

    muestreo de usuarios, botones de edicion y borrado todo ok

--- Frontend/web/usuarios/usuarios.js
+++ Frontend/web/usuarios/usuarios.js
@@ -72,11 +72,36 @@ angular.module('altournative.usuarios', [
 
 		$http({
   		method: 'PUT',
-  		url: 'http://localhost:3000/altournative/usuarios/'+$scope.usuario.items.id,
+  		url: 'http://localhost:3000/altournative/usuarios/',
   		headers: {
    				'x-access-token': store.get('jwt')
  		},
- 		data: $scope.usuarios.Usuarios
+ 		data: $scope.usuario
+
+		}).then(function successCallback(response) {
+
+  		}, function errorCallback(response) {
+ 
+  		});
+  		
+	}
+}).controller('UserDelCtrl', function UsuarioEdicionController($scope, store, $http, jwtHelper) {
+	// $scope.id_usuario = jwtHelper.decodeToken(store.get('jwt')).id;
+	// console.log($scope.id_usuario+'/'+$scope.usuario.items.id+'-'+$scope.usuario.items.login);
+	// if(id_usuario != $scope.usuario.items.id){
+	// 	$('#botonEditar').prop( "disabled", true );
+	// 	console.log(id_usuario+'/'+$scope.usuario.items.id+'-'+$scope.usuario.items.login);
+	// }
+	$scope.borrar = function() {
+		
+
+		$http({
+  		method: 'DELETE',
+  		url: 'http://localhost:3000/altournative/usuarios/'+$scope.usuario.login,
+  		headers: {
+   				'x-access-token': store.get('jwt')
+ 		},
+ 		data: $scope.usuario
 
 		}).then(function successCallback(response) {
 
